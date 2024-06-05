8585e90a3ae32a5a4bf7e1e42e3db386985dc1c5
jacekgawlyta <jac.gawlyta@gmail.com>
2024-06-05 23:30:05 +0200

    Add input elements, button, output label and switch, generate messages and validate form

--- lab1ex1/ViewController.swift
+++ lab1ex1/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var termsSwitch: UISwitch!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func createAccountTapped(_ sender: UIButton) {
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        let confirmPassword = confirmPasswordTextField.text ?? ""
+        let isTermsAccepted = termsSwitch.isOn
+        
+        if email.isEmpty || password.isEmpty || confirmPassword.isEmpty {
+            resultLabel.text = "Wymagane jest podanie adresu email, hasła oraz potwierdzenie hasła"
+        } else if password != confirmPassword {
+            resultLabel.text = "Podane hasła nie są jednakowe"
+        } else if !isTermsAccepted {
+            resultLabel.text = "Wymagana jest akceptacja regulaminu"
+        } else {
+            resultLabel.text = "Gratulacje! Twoje konto zostało utworzone"
+        }
+    }
 }
 
